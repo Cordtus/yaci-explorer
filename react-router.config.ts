56d5526a4b3ee79f595b677d35ac442b27ca375c
Cordt <cordtus@gmail.com>
2025-10-16 10:44:39 -0600

    wip: trying to get addr route discovered by React Router

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,15 +3,4 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
   appDirectory: "src",
-  async routes(defineRoutes) {
-    return defineRoutes((route) => {
-      route("/", "routes/home.tsx");
-      route("/blocks", "routes/blocks.tsx");
-      route("/blocks/:id", "routes/blocks.$id.tsx");
-      route("/transactions", "routes/transactions.tsx");
-      route("/transactions/:hash", "routes/transactions.$hash.tsx");
-      route("/analytics", "routes/analytics.tsx");
-      route("/addr/:id", "routes/addr.$id.tsx");
-    });
-  },
 } satisfies Config;
